497e7bf610b81a233af4b7e4813a2218ea93cb9e
Lina Noguera <lina.noguera@globant.com>
2026-04-09 18:00:22 -0500

    feat:agregar script .sh

---
+++ quien-es-millonario.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Solicita nombre usuario
+echo "Por favor, ingresa tu nombre:"
+read nombre
+
+# Solicita edad
+echo " Ingresa tu edad:"
+read edad
+
+# Imprimir mensaje final
+echo "Hola, $nombre tu tienes $edad años y eres millonario"
